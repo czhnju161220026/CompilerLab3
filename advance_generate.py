37a98abd19cf94f95988922f90681fbd7953bd02
LintianShi <254656131@qq.com>
2019-11-23 22:07:19 -0800

    test

--- advance_generate.py
+++ advance_generate.py
@@ -6,4 +6,4 @@ files = os.listdir(path)
 for f in files:
     if f.endswith('.cmm'):
         #print('./Code/parser ' + path+f + ' > advanced_ir_code/' + f.replace('.cmm', '.ir'))
-        os.system('./Code/parser ' + path+f + ' > advanced_ir_code/' + f.replace('.cmm', '.ir'))
+        os.system('./Code/parser ' + path+f + '   advanced_ir_code/' + f.replace('.cmm', '.ir'))
